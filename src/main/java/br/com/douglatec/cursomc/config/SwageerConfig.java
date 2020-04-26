4fc074ce7107ee2aa9d0db8b43e90ca4f8dd772d
douglas <douglatec@gmail.com>
2020-04-26 19:57:14 -0300

    Testando a primeira classe de dominio - Categoria

---
+++ src/main/java/br/com/douglatec/cursomc/config/SwageerConfig.java
@@ -0,0 +1,52 @@
+package br.com.douglatec.cursomc.config;
+
+import java.util.ArrayList;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.google.common.base.Predicate;
+
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.VendorExtension;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static springfox.documentation.builders.PathSelectors.regex;
+
+@Configuration
+@EnableSwagger2
+public class SwageerConfig {
+	
+	//http://localhost:8080/swagger-ui.html
+	
+	@Bean
+    public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("br.com.douglatec.cursomc"))
+                .paths(regex("/api.*"))
+                .build()
+                .apiInfo(metaInfo());
+    }
+
+  
+	private ApiInfo metaInfo() {
+
+        ApiInfo apiInfo = new ApiInfo(
+                "Produtos API REST",
+                "API REST Projeto Curso MC.",
+                "1.0",
+                "Terms of Service",
+                new Contact("Douglas Oliveita", "https://www.youtube.com",
+                        "douglatec@gmail.com"),
+                "Apache License Version 2.0",
+                "https://www.apache.org/licesen.html", new ArrayList<VendorExtension>()
+        );
+
+        return apiInfo;
+    }
+}
