2ce4c60eb74d2f07d937acc1ff332f1c8bf6305c
douglas <douglatec@gmail.com>
2020-05-04 11:05:09 -0300

    Paginação com parametros opcionais na requisição

--- src/main/java/br/com/douglatec/cursomc/services/CategoriaService.java
+++ src/main/java/br/com/douglatec/cursomc/services/CategoriaService.java
@@ -7,6 +7,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import br.com.douglatec.cursomc.domain.Categoria;
@@ -55,6 +58,11 @@ public class CategoriaService {
 
 	public List<Categoria> findAll(){
 		return categoriaRepository.findAll();
+	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest =PageRequest.of(page, linesPerPage, Direction.valueOf(direction),orderBy);
+		return categoriaRepository.findAll(pageRequest);
 	}
 	
 }
